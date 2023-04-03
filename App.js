6056901a223c0c0b9ae5f9c0d2778472bce86db2
OYBOLATOFF <ramazanoybolatov@gmail.com>
2023-04-03 11:41:02 +0300

    Добавлено меню внизу экрана
    * Чтобы добавить пункт в меню, нужно создать объект списке screens со следующими параметрами:
    1) name - Название
    2) component - .js класс экрана
    3) icon - Название иконки с сайта https://ionic.io/ionicons

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import MainScreen from './screens/MainScreen';
-import LoadingScreen from './screens/LoadingScreen';
 import { useEffect, useState } from 'react';
-import Animated, {FadeInUp, FadeOutDown} from "react-native-reanimated";
+import Animated, {FadeInUp, FadeOutDown, ZoomIn} from "react-native-reanimated";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+// Импортирую окна приложения
+import MainScreen from './screens/MainScreen';
+import InstructionsScreen from './screens/InstructionsScreen';
+import LoadingScreen from './screens/LoadingScreen';
+
+const Tab = createBottomTabNavigator();
+const screens = [
+  {name: 'Посчитать', component: MainScreen, icon: 'aperture'},
+  {name: 'О приложении', component: InstructionsScreen, icon: 'help-circle'},
+]
 
 export default function App() {
   const [isLoading, setLoading] = useState();
@@ -27,7 +39,31 @@ export default function App() {
     )
   }
 
-  return <MainScreen showSplash={showSplash}/>;
+  return (
+    <NavigationContainer>
+      <Animated.View style={{height: '100%'}} entering={ ZoomIn }>
+        <Tab.Navigator screenOptions={{tabBarStyle: {
+          position: 'absolute',
+          bottom: 25,
+          left: 20,
+          right: 20,
+          elevation: 5,
+          backgroundColor: '#00163a',
+          borderRadius: 15,
+          height: 80,
+          paddingVertical: 10,
+          ...styles.shadow
+        }, tabBarActiveTintColor: '#5897ff', tabBarLabelStyle: {fontSize: 15, marginBottom: 10, fontWeight: 'bold'}}}>
+          {screens.map((index, route) => {
+            return <Tab.Screen key={screens.indexOf(this)} options={{ 
+              tabBarIcon: (tabInfo) => (<Icon name={tabInfo.focused? index.icon: `${index.icon}-outline`} size={30} color={ tabInfo.focused? '#5897ff': 'white' } />)
+            }} name={index.name} component={index.component}/>
+          })}
+        </Tab.Navigator>
+      </Animated.View>
+      
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -37,4 +73,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  shadow: {
+    shadowColor: '#003fa7',
+    shadowOffset: {
+        width: 0,
+        height: 20
+    },
+    shadowOpacity: 1,
+    shadowRadius: 10,
+    elevation: 5
+}
 });
--- screens/InstructionsScreen.js
+++ screens/InstructionsScreen.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
-import Icon from 'react-native-vector-icons/Ionicons'
+import { View, Text, Button } from "react-native";
 
 export default ({showSplash}) => {
     return (
         <View style={{height: '100%', alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Страница с инструкцией</Text>
             <Button title="Показать Splash" onPress={showSplash}/>
         </View>
     )
