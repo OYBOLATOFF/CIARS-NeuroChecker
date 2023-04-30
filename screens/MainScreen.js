9aa483d3abcd79a1dd30c244ff0a5904342377aa
OYBOLATOFF <ramazanoybolatov@gmail.com>
2023-04-30 10:29:00 +0300

    Добавлен модуль react-native-vision-camera (но пока не использован)

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
-import Icon from 'react-native-vector-icons/Ionicons'
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default ({showSplash}) => {
     return (
